aff2b65dfe6c07c09daba493b581aa158c233d3c
AliciaPak25 <aliciappak@gmail.com>
2022-02-18 23:16:31 -0300

    agregado de navbar lateral con liberia

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SideNavbar from "./components/SideNavbar";
 
 ReactDOM.render(
   <React.StrictMode>
+    
     <App />
+    <SideNavbar />
   </React.StrictMode>,
   document.getElementById('root')
 );
