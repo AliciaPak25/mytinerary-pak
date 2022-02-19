7ba0443c7eb43816538d0ad7e5943768abc62f8a
AliciaPak25 <aliciappak@gmail.com>
2022-02-18 21:51:18 -0300

    agregado de objeton con ciudades y material ui

---
+++ src/components/citiesData.js
@@ -0,0 +1,48 @@
+const data = {
+    "title": "Popular MYtineraries",
+    "cities": [
+    {
+        "id": 1,
+        "image": "tokyo.jpg",
+        "name": "Tokyo"
+    },
+    {
+        "id": 2,
+        "image": "riodejaneiro.jpg",
+        "name": "Rio de Janeiro"
+    },
+    {
+        "id": 3,
+        "image": "nairobi.jpg",
+        "name": "Nairobi"
+    },
+    {
+        "id": 4,
+        "image": "helsinki.jpg",
+        "name": "Helsinki"
+    },
+    {
+        "id": 5,
+        "image": "denver.jpg",
+        "name": "Denver"
+    },
+    {
+        "id": 6,
+        "image": "stockholm.jpg",
+        "name": "Stockholm"
+    },
+    {
+        "id": 7,
+        "image": "lisbon.jpg",
+        "name": "Lisbon"
+    },
+    {
+        "id": 8,
+        "image": "palermo.jpg",
+        "name": "Palermo"
+    },
+
+]
+}
+
+export default data;
